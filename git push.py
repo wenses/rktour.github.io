98c3b78f9b9830deb7306d15d502fa07f93d1a52
wenses <wenceslausbahati89@gmail.com>
2025-10-24 14:22:18 +0300

    removed navbar

---
+++ git push.py
@@ -0,0 +1,22 @@
+import os, random, string
+
+l=string.ascii_lowercase
+
+def gen_word(size):
+	es=''
+	l=string.ascii_lowercase
+	for i in range(size):
+		es+=random.choice(l)
+
+	return es
+
+try:
+	commit_msg=input("Enter commit msg: ")
+except:
+	commit_msg=random.choice(l)
+
+
+os.system("git add --all")
+os.system(f"git commit -m {commit_msg}")
+os.system("git push origin master")
+os.system("pause")
---
+++ push git changes.bat
@@ -0,0 +1,5 @@
+@echo off
+
+git add --all
+
+git commit -m ""
